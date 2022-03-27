9f92dd8eb267753d80faed972f5bb4a1ee413d73
evelynym <evelynym@outlook.com>
2022-03-27 16:35:01 +0800

    basic finish edit page(without css)

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'http://localhost:5000';
+const url = "http://localhost:5000";
 
 export const fetchExperiments = () => {
-    return axios.get(`${url}/experiments`);
-}
+  return axios.get(`${url}/experiments`);
+};
 
 export const createExperiment = (newPost) => {
-    return axios.post(`${url}/experiments/createExperiment`,newPost)
-}
+  return axios.post(`${url}/experiments/createExperiment`, newPost);
+};
 
 export const deleteExperimentById = (_id) => {
-    return axios.delete(`${url}/experiments/delete/${_id}`)
-}
+  return axios.delete(`${url}/experiments/delete/${_id}`);
+};
 
 export const fetchExperimentByName = (name) => {
-    return axios.get(`${url}/experiments/fetchExperimentByName/${name}`);
-}
+  return axios.get(`${url}/experiments/fetchExperimentByName/${name}`);
+};
 export const isExistsExperimentByName = (name) => {
-    return axios.get(`${url}/experiments/isExistsExperimentByName/${name}`);
-}
+  return axios.get(`${url}/experiments/isExistsExperimentByName/${name}`);
+};
+
+export const updateExperiment = (_id, experiment) => {
+  return axios.patch(`${url}/experiments/updateExperiment/${_id}`, experiment);
+};
 
 export const submitAns = (newAns) => {
-    return axios.post(`${url}/answers/submitAns`,newAns)
-}
-
-
+  return axios.post(`${url}/answers/submitAns`, newAns);
+};
--- src/components/EditExperimentDetailPage.js
+++ src/components/EditExperimentDetailPage.js
@@ -9,20 +9,22 @@ import {
   FormControlLabel,
   Radio,
   Typography,
+  List,
 } from "@mui/material";
 import { fetchExperimentByName } from "../api/index.js";
 import { useNavigate } from "react-router-dom";
+import { updateExperiment } from "../api/index.js";
 
 export default function EditExperimentDetailPage() {
   const { name } = useParams();
   const [experiment, setExperiment] = useState({});
-  const [answers, setAnswers] = useState([]);
+
   const [showAlert, setShowAlert] = useState({
     isOpen: false,
     message: "",
     type: "",
   });
-
+  const defaultType = "singleLine";
   const navigate = useNavigate();
 
   const getData = async (name) => {
@@ -43,17 +45,68 @@ export default function EditExperimentDetailPage() {
     setShowAlert({ isOpen: false, type: "", message: "" });
   };
 
-  const handleUpdateExperiment = () => {};
+  const handleUpdateExperiment = (id) => {
+    updateExperiment(id, experiment);
+  };
 
   const handleBackBtn = () => {
     navigate("/");
+  };
+
+  const handleDeletOptBtn = (optionIndex, questionIndex) => {
+    const list = experiment.questions;
+    list[questionIndex]["questionOptions"].splice(optionIndex, 1);
+    setExperiment({ ...experiment, questions: list });
+  };
+
+  const handleAddOptBtn = (optionIndex, questionIndex) => {
+    const list = experiment.questions;
+    list[questionIndex]["questionOptions"].push([""]);
+    setExperiment({ ...experiment, questions: list });
+  };
+
+  const hanldDeleteQuestionBtn = (questionIndex) => {
+    const list = experiment.questions;
+    list.splice(questionIndex, 1);
+    setExperiment({ ...experiment, questions: list });
+  };
+
+  const handleQuestionNameChange = (event, questionIndex) => {
+    const list = experiment.questions;
+    list[questionIndex]["questionName"] = event.target.value;
+    setExperiment({ ...experiment, question: list });
+  };
+
+  const handleOptTextChange = (event, questionIndex, optionIndex) => {
+    const list = experiment.questions;
+    list[questionIndex]["questionOptions"][optionIndex] = event.target.value;
+    setExperiment({ ...experiment, question: list });
+  };
+
+  const handleAddQuestionBtn = () => {
+    const list = experiment.questions;
+    list.push({
+      questionName: "",
+      questionType: defaultType,
+      questionOptions: ["", ""],
+    });
+    setExperiment({ ...experiment, question: list });
+  };
+
+  const handleExperimentNameChange = (event) => {
+    setExperiment({ ...experiment, experimentName: event.target.value });
+  };
+
+  const handleRadioChange = (event, questionIndex) => {
+    const list = experiment.questions;
+    list[questionIndex]["questionType"] = event.target.value;
+    setExperiment({ ...experiment, question: list });
   };
 
   return (
     <div>
       {Object.keys(experiment).length !== 0 && (
         <div>
-          {console.log(experiment)}
           <Snackbar
             open={showAlert.isOpen}
             autoHideDuration={3000}
@@ -68,34 +121,98 @@ export default function EditExperimentDetailPage() {
             </Alert>
           </Snackbar>
           <Typography>Experiment name</Typography>
-          <TextField value={experiment.experimentName}></TextField>
-          {experiment.questions.map((question, index) => (
-            <div key={index}>
+          <TextField
+            value={experiment.experimentName}
+            onChange={(event) => handleExperimentNameChange(event)}
+          ></TextField>
+          {experiment.questions.map((question, questionIndex) => (
+            <div key={questionIndex}>
+              {console.log(experiment)}
               <Typography>Question</Typography>
 
-              <TextField value={question.questionName}></TextField>
-              <RadioGroup defaultValue={question.questionType}>
-                <FormControlLabel
-                  value="singleLine"
-                  control={<Radio />}
-                  label="Single-line text"
-                />
-                <FormControlLabel
-                  value="multiLine"
-                  control={<Radio />}
-                  label="Multi-line text"
-                />
-                <FormControlLabel
-                  value="MCQ"
-                  control={<Radio />}
-                  label="Multiple Choice"
-                />
-              </RadioGroup>
+              {question.questionName !== "Name" &&
+              question.questionName !== "Email address" &&
+              question.questionName !== "Phone" ? (
+                <div>
+                  <TextField
+                    value={question.questionName}
+                    onChange={(event) =>
+                      handleQuestionNameChange(event, questionIndex)
+                    }
+                  />
+                  <Button onClick={() => hanldDeleteQuestionBtn(questionIndex)}>
+                    Delete question
+                  </Button>
+                  <RadioGroup
+                    onChange={(event) =>
+                      handleRadioChange(event, questionIndex)
+                    }
+                    row
+                    defaultValue={question.questionType}
+                  >
+                    <FormControlLabel
+                      value="singleLine"
+                      control={<Radio />}
+                      label="Single-line text"
+                    />
+                    <FormControlLabel
+                      value="multiLine"
+                      control={<Radio />}
+                      label="Multi-line text"
+                    />
+                    <FormControlLabel
+                      value="MCQ"
+                      control={<Radio />}
+                      label="Multiple Choice"
+                    />
+                  </RadioGroup>
+                </div>
+              ) : (
+                <TextField value={question.questionName} disabled />
+              )}
 
-              {question.questionType === "MCQ" && <div></div>}
+              {console.log(question)}
+              {question.questionType === "MCQ" && (
+                <div>
+                  {question.questionOptions.map((option, optionIndex) => (
+                    <div key={optionIndex}>
+                      <TextField
+                        value={option}
+                        onChange={(event) =>
+                          handleOptTextChange(event, questionIndex, optionIndex)
+                        }
+                      ></TextField>
+                      {question.questionOptions.length > 2 && optionIndex > 1 && (
+                        <div>
+                          <Button
+                            onClick={() =>
+                              handleDeletOptBtn(optionIndex, questionIndex)
+                            }
+                          >
+                            Delete option
+                          </Button>
+                          <Button
+                            onClick={() =>
+                              handleAddOptBtn(optionIndex, questionIndex)
+                            }
+                          >
+                            Add option
+                          </Button>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+              {experiment.questions.length - 1 === questionIndex && (
+                <Button onClick={handleAddQuestionBtn}>Add questions</Button>
+              )}
             </div>
           ))}
-          <Button variant="outlined" onClick={() => handleUpdateExperiment()}>
+          <Button
+            variant="outlined"
+            onClick={() => handleUpdateExperiment(experiment._id)}
+          >
             Update
           </Button>
           <Button variant="outlined" onClick={handleBackBtn}>
